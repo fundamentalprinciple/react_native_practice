efdf133511eb86f5669e5caa571e20e314b2310d
fundamentalprinciple <princedixit931@gmail.com>
2026-01-13 04:15:57 +0000

    now moving on to react native

---
+++ justReact/1/src/Comp1.jsx
@@ -0,0 +1,9 @@
+function Comp1(props) {
+    return (
+        <>
+            <h2>From comp1 component {props.arg}</h2>
+        </>
+    );
+}
+
+export default Comp1
--- justReact/1/src/main.jsx
+++ justReact/1/src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Comp1 from './Comp1.jsx'
+
+var a = ", yo"
+
+const hello = (
+                <>
+                <strictMode>
+                <App />
+                <h2>Using JSX, 5+5={ 5+5 }</h2>
+                </strictMode>
+                <div>I&#39;m a div {a}</div>
+                <Comp1 arg="YO"/>
+                </>
+);
 
 createRoot(document.getElementById('1')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  hello
 )
