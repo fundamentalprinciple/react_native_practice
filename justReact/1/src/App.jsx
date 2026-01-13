ca453ec6a2a81f8309d520b1e06fe602060569b5
fundamentalprinciple <princedixit931@gmail.com>
2026-01-13 03:10:15 +0000

    react refresher

---
+++ justReact/1/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello!</h1>
+            <p>this is a react webapp</p>
+        </div>
+    );
+}
+
+export default App
